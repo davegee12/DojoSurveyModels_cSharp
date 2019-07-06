f5a35443954101ad1d285b6a0af8b98846822789
davegee12 <davidgee1234@gmail.com>
2019-07-06 11:38:43 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyModel.Models;
+
+namespace DojoSurveyModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpPost("create")]
+        public IActionResult Submission(Survey person)
+        {
+            return View("Success", person);
+        }
+        // [HttpGet("result")]
+        // public ViewResult Success()
+        // {
+        //     return View();
+        // }
+    }
+}
---
+++ Models/MyModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DojoSurveyModel.Models
+{
+    public class Survey
+    {
+        public string name{get;set;}
+        public string location{get;set;}
+        public string language{get;set;}
+        public string comment{get;set;}
+    }
+}
